fd22f571bf8d765259e1ddf72efc9ef8efca0b9d
Dave-Patsy <dgwh1995@hotmail.com>
2024-05-13 16:00:57 -0700

    fixed account imports

--- src/app/beethive/account/page.tsx
+++ src/app/beethive/account/page.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import ContentHeader from '@/components/spotify/contentContainer/contentHeader'
-import { getServerAuthSession } from '@/server/auth'
 import { api } from '@/trpc/server';
 import Subscription from './_components/subscription';
 
 export default async function Page() {
-  const session = await getServerAuthSession()
+
   const subscription = await api.beethive.beethiveStripeRouter.getSubscription()
 
+  
   return (
     <div>
       <div className='pl-8 pt-8 space-y-4'>
