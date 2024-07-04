c2563b886d625e90a81090833ef15908cb04fddc
Dave-Patsy <dgwh1995@hotmail.com>
2024-07-04 12:15:09 -0700

    Update sanity-layout.tsx

--- src/components/sanity/sanity-layout.tsx
+++ src/components/sanity/sanity-layout.tsx
@@ -2,7 +2,7 @@
 import { Stack, Card, Flex, Text, } from "@sanity/ui";
 
 /* eslint-disable */
-export default function MyActiveToolLayout(props) {
+export default function MyActiveToolLayout(props: { renderDefault: (arg0: any) => any; }) {
 
   return (
     <div className="h-full">
