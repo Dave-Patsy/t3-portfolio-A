d756dd5d7fafb9ebd906121cbd5250ca7b127387
Dave-Patsy <dgwh1995@hotmail.com>
2024-09-16 03:43:45 -0700

    asasd

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -18,6 +18,7 @@ export const { handlers, signIn, signOut, auth,unstable_update } = NextAuth({
     signIn: "/auth/login",
     error: "/auth/error",
   },
+  // useSecureCookies:true,
   adapter: PrismaAdapter(db) as Adapter,
   events: {
     async linkAccount({ user }) {
